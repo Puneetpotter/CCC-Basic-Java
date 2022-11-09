4ece8423b6fb78d144b66b62d286e07b64953cc0
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2022-11-10 02:15:18 +0530

    Create CustomizedWelcomeMessage.java

---
+++ Keywords-1/CustomizedWelcomeMessage.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner s= new
+    Scanner(System.in);
+    String name = s.nextLine();
+    System.out.printf("Hello %s ! Welcome to Examly Event Management System",name);
+  }
+}
