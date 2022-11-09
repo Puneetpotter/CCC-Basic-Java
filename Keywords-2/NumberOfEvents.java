7d9ee6ebb4d47f2728c949c6429b10e75a9cd310
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2022-11-10 01:22:48 +0530

    Create NumberOfEvents.java

---
+++ Keywords-2/NumberOfEvents.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner s= new
+    Scanner(System.in);
+    int events = s.nextInt();
+    System.out.printf("Number of events hosted in Codissia is %d",events);
+  }
+}
